c7122967c20b0d8988217e2458230caef6c691ab
SEONGEUN0415 <wltnr1998@gmail.com>
2022-08-29 18:43:34 +0900

    JS: Home contact button

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict' 
 
 // Make navbar transparent when it is on the top
 const navbar = document.querySelector('#navbar');
@@ -10,7 +10,27 @@ const navbarHeight = navbar.getBoundingClientRect().height;
         navbar.classList.add('navbar--appear');
     } else {
         navbar.classList.remove('navbar--appear');
-    }
-   
+    } 
 });
-  
+
+// Handle scrolling when tapping on the navbar menu
+const navabrMenu = document.querySelector('.navbar__menu');
+navabrMenu.addEventListener('click' , (event) => {
+    const target = event.target;
+    const link = target.dataset.link;
+    if (link == null) {
+        return;
+    } 
+        scrollIntoView(link);
+})
+
+// Handle scrolling when 'contact me' is pressed.
+const contactMe = document.querySelector('.home__contact');
+contactMe.addEventListener('click' , (event) => {
+    scrollIntoView('#contact');
+})
+
+function scrollIntoView(selector) {
+    const scrollTo = document.querySelector(selector);
+    scrollTo.scrollIntoView({behavior: "smooth"}); 
+}
